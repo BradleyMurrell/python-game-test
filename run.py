dff87d1cc3062a77bb61750ecd3d8018d2b3890f
BradleyMurrell <b.murrell89@gmail.com>
2022-03-01 19:13:01 +0000

    Add heading and name input

--- run.py
+++ run.py
@@ -1,8 +1,16 @@
 game_start = "yes"
 
+print("-" * 35)
+print("A messengers journey")
+print("-" * 35)
+player_name = input("Please enter your name: ")
+print("-" * 35)
+
 while game_start == "yes":
-    print("You come to a crossroad. Do you go left or right?")
+    print(f"{player_name} comes to a crossroad. Do they go left or right?")
     answer_1 = input("left or right? ")
+
+    print("Travelling...")
 
     if answer_1 == "left":
         print("You come across a merchant. Do you stop and talk to the merchant?")
@@ -11,9 +19,9 @@ while game_start == "yes":
         bridge = input("yes or no? ")
         while bridge == "no":
             print("There is no other way around. Do you cross the bridge?")
-            bridge = input("yes or no?")
+            bridge = input("yes or no? ")
         print("You cross the bridge. It starts to fall apart. Do you make a run for it?")
-    answer_2
+    
 
 
     game_start = input("Play again: ")
