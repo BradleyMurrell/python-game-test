409048da0b58d233d55239526b30afb9c3c954eb
BradleyMurrell <b.murrell89@gmail.com>
2022-03-01 18:46:28 +0000

    Add content for first answer

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
 game_start = "yes"
 
 while game_start == "yes":
-    print("Begin journey. Yes or no?")
-    answer_1 = input("yes or no? ")
+    print("You come to a crossroad. Do you go left or right?")
+    answer_1 = input("left or right? ")
+
+    if answer_1 == "left":
+        print("You come across a merchant. Do you stop and talk to the merchant?")
+    else:
+        print("You come across a bridge. Do you cross the bridge?")
+        bridge = input("yes or no? ")
+        while bridge == "no":
+            print("There is no other way around. Do you cross the bridge?")
+            bridge = input("yes or no?")
+        print("You cross the bridge. It starts to fall apart. Do you make a run for it?")
+    answer_2
+
 
     game_start = input("Play again: ")
