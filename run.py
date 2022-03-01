0edf9a96e73bde5c11b0594b2d8cfb7f7062e841
BradleyMurrell <b.murrell89@gmail.com>
2022-03-01 20:22:54 +0000

    Add answer 2a and 2b

--- run.py
+++ run.py
@@ -14,6 +14,12 @@ while game_start == "yes":
 
     if answer_1 == "left":
         print("You come across a merchant. Do you stop and talk to the merchant?")
+        answer_2_a = input("yes or no? ")
+        if answer_2_a == "yes":
+            print("The merchant shows you his wares")
+        else:
+            print("You continue your journey")
+
     else:
         print("You come across a bridge. Do you cross the bridge?")
         bridge = input("yes or no? ")
@@ -21,6 +27,11 @@ while game_start == "yes":
             print("There is no other way around. Do you cross the bridge?")
             bridge = input("yes or no? ")
         print("You cross the bridge. It starts to fall apart. Do you make a run for it?")
+        answer_2_b = input("yes or no? ")
+        if answer_2_b == "yes":
+            print("You make it across just in time")
+        else:
+            print("You fall to your death")
     
 
 
