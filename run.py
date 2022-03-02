9ea9e16088e09eb411d0c53c00e0aa20bc3ae062
BradleyMurrell <b.murrell89@gmail.com>
2022-03-02 21:56:36 +0000

    Add a more detailed story to the game start

--- run.py
+++ run.py
@@ -3,25 +3,32 @@ game_start = "yes"
 print("-" * 35)
 print("A messengers journey")
 print("-" * 35)
-player_name = input("Please enter your name: ")
+character_name = input("Please enter your character's name: ")
 print("-" * 35)
 
 while game_start == "yes":
-    print(f"{player_name} comes to a crossroad. Do they go left or right?")
-    answer_1 = input("left or right? ")
+    print(f"A messenger named {character_name} has been tasked to deliver an important message to the castle.")
+    print(f"Along the way, {character_name} came to a crossroad. To the left, the path led through mountains.")
+    print(f"To the right, the path led through a dark and scary forest.")
+    print(f"The mountains look safer, but will probably take a few extra days to reach the castle.")
+    print(f"The scary forest would be quicker, but it looks dangerous. Does {character_name} go left or right?")
+    print("-" * 35)
+    answer_1 = input("[Enter: left or right?] ")
+    print("-" * 35)
 
     print("Travelling...")
 
+    print("-" * 35)
     if answer_1 == "left":
         print("You come across a merchant. Do you stop and talk to the merchant?")
-        answer_2_a = input("yes or no? ")
+        answer_2_a = input("[Enter: left or right?] ")
         if answer_2_a == "yes":
             print("The merchant is actually a ninja and kills you")
             print("Game over")
         if answer_2_a == "no":
             print("You continue your journey.")
             print("You walk past a lake. Do you go fishing?")
-            answer_3_a = input("yes or no? ")
+            answer_3_a = input("[Enter: left or right?] ")
             if answer_3_a == "yes":
                 print("You catch a fish and eat it")
                 print("Now that you are well rested, you continue your journey")
@@ -33,16 +40,16 @@ while game_start == "yes":
 
     if answer_1 == "right":
         print("You come across a bridge. Do you cross the bridge?")
-        bridge = input("yes or no? ")
+        bridge = input("[Enter: left or right?] ")
         while bridge == "no":
             print("There is no other way around. Do you cross the bridge?")
-            bridge = input("yes or no? ")
+            bridge = input("[Enter: left or right?] ")
         print("You cross the bridge. It starts to fall apart. Do you make a run for it?")
-        answer_2_b = input("yes or no? ")
+        answer_2_b = input("[Enter: left or right?] ")
         if answer_2_b == "yes":
             print("You make it across just in time")
             print("You walk into a troll. Do you attack it?")
-            answer_3_b = input("yes or no? ")
+            answer_3_b = input("[Enter: left or right?] ")
             if answer_3_b == "yes":
                 print("You attack the troll and kill it")
                 print("You continue your journey and make it to the castle to deliver your message")
